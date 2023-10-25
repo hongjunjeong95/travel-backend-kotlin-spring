04ba75c19f3585e4f4fc4e57b8b5ca4fb5999ead
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-25 22:15:29 +0900

    Amend swagger

--- src/main/kotlin/com/travel/travelapp/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/travel/travelapp/common/config/SwaggerConfig.kt
@@ -12,14 +12,20 @@ import org.springframework.context.annotation.Configuration
 @OpenAPIDefinition(info = Info(title = "Travel App API 명세서", description = "여행 API 명세서", version = "v1"))
 @Configuration
 class SwaggerConfig {
+    private val SECURITY_SCHEME_NAME = "Authorization"
+
     @Bean
     fun openAPI(): OpenAPI {
         val securityScheme: SecurityScheme = SecurityScheme()
-            .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-            .`in`(SecurityScheme.In.HEADER).name("Authorization")
+            .name(SECURITY_SCHEME_NAME)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
         val securityRequirement: SecurityRequirement = SecurityRequirement().addList("bearerAuth")
         return OpenAPI()
-            .components(Components().addSecuritySchemes("bearerAuth", securityScheme))
+            .components(Components().addSecuritySchemes(SECURITY_SCHEME_NAME, securityScheme))
+            .addSecurityItem(SecurityRequirement().addList(SECURITY_SCHEME_NAME))
             .security(listOf(securityRequirement))
     }
 }
