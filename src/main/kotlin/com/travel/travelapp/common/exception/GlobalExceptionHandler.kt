19c8e6ff9b2099e6e32114af3f86984ada3c2c18
hongjunjeong <hongjunjeong95@gmail.com>
2023-10-29 17:14:34 +0900

    Throw error on global excpetion error

--- src/main/kotlin/com/travel/travelapp/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/travel/travelapp/common/exception/GlobalExceptionHandler.kt
@@ -4,6 +4,8 @@ import com.auth0.jwt.exceptions.JWTDecodeException
 import com.auth0.jwt.exceptions.TokenExpiredException
 import com.auth0.jwt.exceptions.SignatureVerificationException
 import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -13,37 +15,37 @@ class GlobalExceptionHandler {
     private val logger = KotlinLogging.logger {}
 
     @ExceptionHandler(ServerException::class)
-    fun handleServerException(ex: ServerException) : ErrorResponse {
+    fun handleServerException(ex: ServerException): ResponseEntity<ErrorResponse> {
         logger.error { ex.message }
 
-        return ErrorResponse(code = ex.code, message = ex.message)
+        return ResponseEntity.status(ex.code).body(ErrorResponse(code = ex.code, message = ex.message))
     }
 
     @ExceptionHandler(SignatureVerificationException::class)
-    fun handleSignatureVerificationException(ex: SignatureVerificationException) : ErrorResponse {
+    fun handleSignatureVerificationException(ex: SignatureVerificationException): ResponseEntity<ErrorResponse> {
         logger.error { ex.message }
 
-        return ErrorResponse(code = 401, message = "Token is not verified")
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse(code = 401, message = "Token is not verified"))
     }
 
     @ExceptionHandler(JWTDecodeException::class)
-    fun handleJWTDecodeException(ex: JWTDecodeException) : ErrorResponse {
+    fun handleJWTDecodeException(ex: JWTDecodeException): ResponseEntity<ErrorResponse> {
         logger.error { ex.message }
 
-        return ErrorResponse(code = 401, message = "Token is null")
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse(code = 401, message = "Token is null"))
     }
 
     @ExceptionHandler(TokenExpiredException::class)
-    fun handleTokenExpiredException(ex: TokenExpiredException) : ErrorResponse {
+    fun handleTokenExpiredException(ex: TokenExpiredException): ResponseEntity<ErrorResponse> {
         logger.error { ex.message }
 
-        return ErrorResponse(code = 401, message = "Token Expired Error")
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse(code = 401, message = "Token Expired Error"))
     }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(ex: Exception) : ErrorResponse {
+    fun handleException(ex: Exception): ResponseEntity<ErrorResponse> {
         logger.error { ex.message }
 
-        return ErrorResponse(code = 500, message = "Internal Server Error")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse(code = 500, message = "Internal Server Error"))
     }
 }
